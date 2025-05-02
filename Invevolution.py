4fa4484149572a8c69f7362b87b1673410f122e9
Jinge Zuo <202208108+JingeZuo@users.noreply.github.com>
2025-05-02 17:02:17 +0800

    Add files via upload

--- Invevolution.py
+++ Invevolution.py
@@ -1,6 +1,20 @@
 import numpy as np
-import matplotlib.pyplot as plt
-#%%
+import itertools
+def generate_binary_combinations(input_matrix):
+    # 输入矩阵应为m*1的numpy数组或列表
+    m = len(input_matrix)  # 获取输入矩阵的行数m
+    ones_indices = [i for i, val in enumerate(input_matrix) if val == 1]
+    n = len(ones_indices)
+    all_combinations = []
+
+    # 生成1到n个元素的组合
+    for k in range(0, n + 1):
+        for combo in itertools.combinations(ones_indices, k):
+            matrix = np.zeros((m, 1), dtype=int)  # 动态创建m*1的零矩阵
+            matrix[list(combo), 0] = 1  # 明确指定列索引为0
+            all_combinations.append(matrix)
+
+    return all_combinations
 def XInvevolution(A):
     C=[15,1,2,4,8,16]
     T=[3,5,6,7,9,10,11,12,13,14,15]
@@ -18,8 +32,7 @@ def XInvevolution(A):
         EV = np.hstack((EV, B))
     return EV
 A=np.array([[1],[0],[1],[0],[1],[0],[1],[0],[1],[0],[1],[0],[1],[0],[1],[0]])
-print(XInvevolution(A))
-#%%
+
 def ZInvevolution(A):
     C=[15,1,2,4,8,16]
     T=[3,5,6,7,9,10,11,12,13,14,15]
@@ -38,9 +51,15 @@ def ZInvevolution(A):
         B[C[i]-1][0]=(B[C[i]-1][0]+s)%2
         EV=np.hstack((EV,B))
     return EV
-A=np.array([[0],[1],[1],[0],[0],[1],[1],[0],[0],[1],[1],[0],[0],[1],[1],[0]])
-print(ZInvevolution(A))
+A=np.array([[1],[0],[1],[0],[1],[0],[1],[0],[1],[0],[1],[0],[1],[0],[1],[0]])
+XEV=XInvevolution(A)
 
+ZEV=ZInvevolution(A)
+Z=np.array(generate_binary_combinations(A))
+bra=[]
 
-
-
+for i in range(len(generate_binary_combinations(A))):
+    S=np.array(ZInvevolution(Z[i]))
+    bra.append(np.vstack((XEV, S)))
+bra=np.array(bra)
+print(bra.shape)
