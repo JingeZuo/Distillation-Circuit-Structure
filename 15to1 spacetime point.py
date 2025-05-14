50d1379910c9e11208540ebb716eb7c9ab5c499f
Jinge Zuo <202208108+JingeZuo@users.noreply.github.com>
2025-05-14 20:50:31 +0800

    Add files via upload

--- 15to1 spacetime point.py
+++ 15to1 spacetime point.py
@@ -1,10 +1,10 @@
 import qiskit
 from matplotlib import pyplot as plt
 from qiskit import QuantumCircuit, transpile
-from qiskit.quantum_info import Clifford, Statevector
+from qiskit.quantum_info import Clifford, Statevector, DensityMatrix
 from qiskit_aer import AerSimulator
 import numpy as np
-
+from qiskit.quantum_info import SuperOp,Statevector,Pauli,DensityMatrix,Operator,partial_trace,Clifford,PTM,Choi
 q1= qiskit.QuantumRegister(1, '|0>\u2081')
 q2= qiskit.QuantumRegister(1, '|0>\u2082')
 q3= qiskit.QuantumRegister(1, '|0>\u2083')
@@ -22,66 +22,95 @@ q14= qiskit.QuantumRegister(1, '|0>\u2081\u2084')
 q15= qiskit.QuantumRegister(1, '|0>\u2081\u2085')
 q16= qiskit.QuantumRegister(1, '|0>\u2081\u2086')
 
-c1=qiskit.ClassicalRegister(1,'c1')
-c2=qiskit.ClassicalRegister(1,'c2')
-c3=qiskit.ClassicalRegister(1,'c3')
-c4=qiskit.ClassicalRegister(1,'c4')
-c5=qiskit.ClassicalRegister(1,'c5')
-c6=qiskit.ClassicalRegister(1,'c6')
-c7=qiskit.ClassicalRegister(1,'c7')
-c8=qiskit.ClassicalRegister(1,'c8')
-c9=qiskit.ClassicalRegister(1,'c9')
-c10=qiskit.ClassicalRegister(1,'c10')
-c11=qiskit.ClassicalRegister(1,'c11')
-c12=qiskit.ClassicalRegister(1,'c12')
-c13=qiskit.ClassicalRegister(1,'c13')
-c14=qiskit.ClassicalRegister(1,'c14')
-c15=qiskit.ClassicalRegister(1,'c15')
-c16=qiskit.ClassicalRegister(1,'c16')
-qp=qiskit.QuantumRegister(1,'qp')
-cp=qiskit.ClassicalRegister(1,'cp')
-qc=QuantumCircuit(q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15,q16,qp,
-                  c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,cp)
-qreg=[q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15]
-creg=[c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15]
-qc.h([0,1,3,7,15])
+qc=QuantumCircuit(q16,q15,q14,q13,q12,q11,q10,q9,q8,q7,q6,q5,q4,q3,q2,q1)
+
+qc.h([15,14,12,8,0])
 qc.barrier()
-qc.cx(15,14)
+qc.cx(0,1)
 qc.barrier()
-for i in [8,9,10,11,12,13,14]:
-    qc.cx(7,i)
+for i in [7,6,5,4,3,2,1]:
+    qc.cx(8,i)
 qc.barrier()
-for i in [4,5,6,11,12,13,14]:
-    qc.cx(3,i)
+for i in [11,10,9,4,3,2,1]:
+    qc.cx(12,i)
 qc.barrier()
-for i in [2,5,6,9,10,13,14]:
-    qc.cx(1,i)
-qc.barrier()
-for i in [2,4,6,8,10,12,14]:
-    qc.cx(0,i)
-qc.barrier()
-for i in [2,4,5,8,9,11]:
+for i in [13,10,9,6,5,2,1]:
     qc.cx(14,i)
 qc.barrier()
-allqubit=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+for i in [13,11,9,7,5,3,1]:
+    qc.cx(15,i)
+qc.barrier()
+for i in [13,11,10,7,6,4]:
+    qc.cx(1,i)
+qc.barrier()
+allqubit=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 qc.tdg(allqubit)
-g=[[3,4,5,6,7,8,9,10],
-   [0,1,2,3,4,5,6,14],
-   [1,2,3,4,9,10,11,12],
-   [0,1,4,5,8,9,12,13]]
-qc.h(allqubit)
-for q_reg, c_reg in zip(qreg, creg):
-    qc.measure(q_reg[0], c_reg[0])
-for c in creg:
-    with qc.if_test((c, 1)):  # 新版条件语法（寄存器对象 + 目标值）
-        qc.x(qp)
-qc.measure(qp,cp)
-with qc.if_test((cp,1)):
-    qc.z(q16)
-qc.tdg(15)
-qc.h(15)
-qc.measure(q16,c16)
- # q_reg[0] 是唯一的量子位，c_reg[0] 是唯一的经典位
+g=[[12,11,10,9,8,7,6,5],
+   [15,14,13,12,11,10,9,1],
+   [14,13,12,11,6,5,4,3],
+   [15,14,11,10,7,6,3,2]]
+
+state0=Statevector.from_instruction(qc)
+qc.barrier()
+
+#%%
+#stablizer1
+qc.x(g[0])
+state1=(state0+Statevector.from_instruction(qc))/2
+qc.barrier()
+
+#%%
+#stablizer2
+qc.x(g[1])
+state2=(state1+Statevector.from_instruction(qc))/2
+qc.barrier()
+
+#%%
+#stablizer3
+qc.x(g[2])
+state3=(state2+Statevector.from_instruction(qc))/2
+qc.barrier()
+
+#%%
+#stablizer4
+qc.x(g[3])
+state4=(state3+Statevector.from_instruction(qc))/2
+qc.barrier()
+
+#%%
+#Logical X+1
+qc.x(allqubit)
+state=Statevector.from_instruction(qc)
+stateLplus=(state4+state)/2
+qc.barrier()
+normLplus=np.linalg.norm(stateLplus.data)
+
+#%%
+#Logical X-1
+stateLminus=(state4-state)/2
+qc.z(0)
+normLminus=np.linalg.norm(stateLminus.data)
+stateLminusZ=normLminus*Statevector.from_instruction(qc)
+normLminusZ=np.linalg.norm(stateLminusZ.data)
+
+#%%
+# 定义矩阵
+U = np.array([
+    [0, np.exp(-1j * np.pi / 4)],
+    [np.exp(1j * np.pi / 4), 0]
+])
+
+# 创建 Operator 对象
+eigenT= Operator(U)
+qc.unitary(eigenT,0,label='EigenT')
+qc.barrier()
+qc.draw()
+plt.show()
+#%%
+is_equiv = stateLplus.equiv(Statevector.from_instruction(qc))
+print(f"等价性判断: {is_equiv}")
+#%%
+
 # 3. 编译并运行电路
 # ---------------------------
 # 创建模拟器后端
@@ -92,8 +121,7 @@ transpiled_circuit = transpile(qc, backend)  # 关键修复点
 job = backend.run(transpiled_circuit, shots=20)
 result = job.result()
 counts = result.get_counts()
-qc.draw()
-plt.show()
+
 # ---------------------------
 # 4. 结果输出
 # ---------------------------
@@ -103,11 +131,11 @@ counts_list = [[int(c) for c in key.split()] for key in counts]
 for i in range(len(counts_list)):
     l=0
     for j in range(len(counts_list[i])-1):
-        l=l+counts_list[i][len(counts_list[i])-1-j]
+        l=l+counts_list[i][j]
     for m in range(len(g)):
         s = 0
         for n in range(len(g[m])):
-            s=s+counts_list[i][len(counts_list[i])-1-g[m][n]]
+            s=s+counts_list[i][g[m][n]]
         print('s',m,s%2)
     print(l%2)
 
